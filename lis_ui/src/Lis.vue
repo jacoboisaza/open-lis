0a61cca40474f75a7a452a52c0965ec3eea14b71
jacoboisaza <jacoboisaza@gmail.com>
2020-04-29 23:19:27 -0500

    [user] Implement Vue blank template

---
+++ lis_ui/src/Lis.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app>
+    <!-- <lis-snackbar /> -->
+    <!-- <lis-navbars /> -->
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Lis",
+  data: () => ({})
+};
+</script>
+
+<style>
+/* Google font: roboto 100, 300, regular, 500, 700, 900 - latin */
+@font-face {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 100;
+  src: local("Roboto Thin"), local("Roboto-Thin"),
+    url(./assets/fonts/roboto-v20-latin-100.woff2) format("woff2");
+}
+@font-face {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 300;
+  src: local("Roboto Light"), local("Roboto-Light"),
+    url(./assets/fonts/roboto-v20-latin-300.woff2) format("woff2");
+}
+@font-face {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  src: local("Roboto"), local("Roboto-Regular"),
+    url(./assets/fonts/roboto-v20-latin-regular.woff2) format("woff2");
+}
+@font-face {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 500;
+  src: local("Roboto Medium"), local("Roboto-Medium"),
+    url(./assets/fonts/roboto-v20-latin-500.woff2) format("woff2");
+}
+@font-face {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  src: local("Roboto Bold"), local("Roboto-Bold"),
+    url(./assets/fonts/roboto-v20-latin-700.woff2) format("woff2");
+}
+@font-face {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 900;
+  src: local("Roboto Black"), local("Roboto-Black"),
+    url(./assets/fonts/roboto-v20-latin-900.woff2) format("woff2");
+}
+</style>
--- lis_ui/src/main.js
+++ lis_ui/src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import App from "./App.vue";
+import Lis from "./Lis.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
@@ -9,9 +9,18 @@ import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
+// Set global config variables
+Vue.prototype.$configs = {};
+Vue.prototype.$configs.adminMail = "covidopenlis@gmail.com";
+if (process.env.NODE_ENV === "production") {
+  Vue.prototype.$configs.lisAuthBaseUrl = "http://localhost:3000/";
+} else {
+  Vue.prototype.$configs.lisAuthBaseUrl = "http://localhost:3000/";
+}
+
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(Lis)
+}).$mount("#lis");
